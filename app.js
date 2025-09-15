7815d3f3411bc0d51d72b19621621bff641134af
DanishNazakat <danishnazakat636@gmail.com>
2025-09-14 17:07:40 -0700

    complete Todo App

--- app.js
+++ app.js
@@ -1,36 +1,46 @@
-function addTodos(){
-    let userinp = document.getElementById('userInp').value;
+function addTodos() {
+    let userinp = document.getElementById('userInp');
     let result = document.querySelector('.reault');
 
+    if (userinp.value === "") {
+        alert('Enter some Text')
+    }
+    else {
+        let todos = document.createElement('div');
+        todos.setAttribute('class', 'todos');
+        result.append(todos);
 
-    let todos = document.createElement('div');
-    todos.setAttribute('class','todos');
-    result.append(todos);
-
-    let todosText = document.createElement('div');
-    todosText.innerText = userinp;
-    todos.appendChild(todosText);
-
-
-    // Delet button
-    let deletButton = document.createElement('button');
-    deletButton.innerText = "delete";
-    // deletButton.setAttribute('onClick',`function updateValue(){ console.log(todos) }updateValue()`)
-    todos.appendChild(deletButton);
-    deletButton.onclick = function () {
-        todos.remove();
-    };
+        let todosText = document.createElement('div');
+        todosText.innerText = userinp.value;
+        todos.appendChild(todosText);
 
 
+        // edit and delete div
+        let editDelete = document.createElement('div');
+        todos.append(editDelete);
 
-    
-    // Delet button
-    let editButton = document.createElement('button');
-    editButton.innerText = "Edit";
-    // deletButton.setAttribute('onClick',`function updateValue(){ console.log(todos) }updateValue()`)
-    todos.appendChild(editButton);
-    editButton.onclick = function () {
-        let newValue = prompt('Enter New Value');
-        todosText.innerText = newValue;        
-    };
+        // Delet button
+        let deletButton = document.createElement('button');
+        deletButton.setAttribute('class','deleteButton')
+        deletButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
+        // deletButton.setAttribute('onClick',`function updateValue(){ console.log(todos) }updateValue()`)
+        editDelete.appendChild(deletButton);
+        deletButton.onclick = function () {
+            todos.remove();
+        };
+
+
+        // 
+        // Edit  button
+        let editButton = document.createElement('button');
+        editButton.setAttribute('class','editButton')
+        editButton.innerHTML = '<i class="fa-solid fa-pen"></i>';
+        // deletButton.setAttribute('onClick',`function updateValue(){ console.log(todos) }updateValue()`)
+        editDelete.appendChild(editButton);
+        editButton.onclick = function () {
+            let newValue = prompt('Enter New Value');
+            todosText.innerText = newValue;
+        };
+        userinp.value = "";
+    }
 }
